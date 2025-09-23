093bd78d66ca87817e25bd33f08292c85b5c33fe
AditL04 <adwitiyalaha04@gmail.com>
2025-09-24 00:20:52 +0530

    Add existing sikkim-frontend files into Harshita-feature

---
+++ Harshita-feature/data.js
@@ -0,0 +1,15 @@
+const monasteries = [
+    {
+        id: 1,
+        name: 'Rumtek Monastery',
+        audioSrc: './audios/rumtek.mp3',
+        transcript: 'Rumtek Monastery is one of the most important monasteries in Sikkim...'
+    },
+    {
+        id: 2,
+        name: 'Pemayangtse Monastery',
+        audioSrc: './audios/pemayangtse.mp3',
+        transcript: 'Pemayangtse Monastery is famous for its architecture and history...'
+    },
+    // ... Add all 32 monasteries in similar format
+];
