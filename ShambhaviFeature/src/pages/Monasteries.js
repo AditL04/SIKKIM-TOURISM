661ce49fbbaf0d653644fe8aa55d97b30d892198
Shambhavi-byte <guptashambhavi26@gmail.com>
2025-09-23 22:37:06 +0530

    Remove ShambhaviFeature from index

--- ShambhaviFeature/src/pages/Monasteries.js
+++
@@ -1,9 +0,0 @@
-// src/pages/Monasteries.js
-import React from "react";
-import MonasteryExplorer from "../components/MonasteryExplorer";
-
-function Monasteries() {
-  return <MonasteryExplorer />;
-}
-
-export default Monasteries;
